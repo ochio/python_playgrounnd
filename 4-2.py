4997b332a78252a718e91fc62afdc3ae61deecf4
ochi <ocos54ce@gmail.com>
2022-06-18 20:17:22 +0900

    4-2

--- 4-2.py
+++ 4-2.py
@@ -1,4 +1,17 @@
 import numpy as np
+import sys
+import os
+sys.path.append(os.pardir)
+from dataset.mnist import load_mnist
+
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+
+train_size = x_train.shape[0]
+batch_size = 10
+batch_mask = np.random.choice(train_size, batch_size)
+x_batch = x_train[batch_size]
+t_batch = t_train[batch_size]
+print([np.arange(3), np.array([1, 23, 0])])
 
 
 def mean_squared_error(y, t):
@@ -6,10 +19,10 @@ def mean_squared_error(y, t):
     return 0.5 * np.sum((y - t)**2)
 
 
-t = np.array([0, 0, 1, 0, 0, 0, 0, 0, 0, 0])
-y = np.array([0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0])
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
 
-print(mean_squared_error(y, t))
-
-y = np.array([0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.0, 0.0])
-print(mean_squared_error(y, t))
+    batch_size = y.shape[0]
+    return -np.sum(t * np.log(y + 1e-7)) / batch_size
