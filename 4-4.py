434cb66c0077502089d8f874bad472722b419b54
ochi <ocos54ce@gmail.com>
2022-06-24 19:49:10 +0900

    4-4

--- 4-4.py
+++ 4-4.py
@@ -10,10 +10,6 @@ def function_1(x):
     return 0.01 * x**2 + 0.1 * x
 
 
-def function_2(x):
-    return x[0] ** 2 + x[1] ** 2
-
-
 def numerical_gradient(f, x):
     h = 1e-4
     grad = np.zeros_like(x)
@@ -32,6 +28,19 @@ def numerical_gradient(f, x):
     return grad
 
 
-print(numerical_gradient(function_2, np.array([3.0, 4.0])))
-print(numerical_gradient(function_2, np.array([0.0, 2.0])))
-print(numerical_gradient(function_2, np.array([3.0, 0.0])))
+def gradient_descent(f, init_x, lr=0.01, step_num=100):
+    x = init_x
+
+    for i in range(step_num):
+        grad = numerical_gradient(f, x)
+        x -= lr * grad
+
+    return x
+
+
+def function_2(x):
+    return x[0] ** 2 + x[1] ** 2
+
+
+init_x = np.array([-3.0, 4.0])
+print(gradient_descent(function_2, init_x=init_x, lr=0.1, step_num=100))
